71eea2cc1d1a44df5178f257a979ee0040c5c0f0
emreceylan71 <109828939+emreceylan71@users.noreply.github.com>
2022-08-12 21:34:18 +0300

    Add files via upload

---
+++ String_exercises.cpp
@@ -0,0 +1,16 @@
+﻿
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	string letter1, letter2;
+	cout << "Give us a letter " << endl;
+	cin >> letter1;
+
+	for (int i =letter1.length() ; i >=0 ; i--) {
+
+		cout << letter1[i];
+
+	}
+}
